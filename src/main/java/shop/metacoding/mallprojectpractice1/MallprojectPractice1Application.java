17f13ccda5be798bee4c21b95e03427cc3e33227
Jeongyusu <Jeongyusu@naver.com>
2023-08-01 21:51:35 +0900

    depandancy, yml 세팅완료

---
+++ src/main/java/shop/metacoding/mallprojectpractice1/MallprojectPractice1Application.java
@@ -0,0 +1,13 @@
+package shop.metacoding.mallprojectpractice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallprojectPractice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallprojectPractice1Application.class, args);
+	}
+
+}
---
+++ src/test/java/shop/metacoding/mallprojectpractice1/MallprojectPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.metacoding.mallprojectpractice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallprojectPractice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
